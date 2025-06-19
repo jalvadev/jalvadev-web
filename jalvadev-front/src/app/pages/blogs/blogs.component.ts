1dbb6f5cfe22e2200c9995018684ce5d3fb3493e
Jose Alvarez <josealvarezvivar@gmail.com>
2025-06-19 21:19:12 +0200

    feature(translation): add i18n

--- jalvadev-front/src/app/pages/blogs/blogs.component.ts
+++ jalvadev-front/src/app/pages/blogs/blogs.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { HttpService } from '../../services/http.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-blogs',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './blogs.component.html',
   styleUrl: './blogs.component.less'
 })
@@ -12,6 +13,7 @@ export class BlogsComponent implements OnInit{
   http: HttpService = inject(HttpService);
 
   ngOnInit(): void {
+    debugger;
     this.http.get("http://localhost:8080/api/User");
   }
 }
--- jalvadev-front/src/app/services/http.service.ts
+++ jalvadev-front/src/app/services/http.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private translate: TranslateService) { }
 
   async get(url: string): Promise<any> {
     try{
+      debugger;
       const response = await fetch(url);
       if(!response.ok)
-        return new Promise<any>((resolve, reject) => { resolve({ error: "Error processing the call." }) });
+        return new Promise<any>((resolve, reject) => { resolve({ error: this.translate.instant("HTTP_KO") }) });
 
       const data = await response.json();
       return data;
@@ -19,7 +21,7 @@ export class HttpService {
       console.log(error.message);
       return new Promise<any>((resolve, reject) => {
         resolve({
-          error: "An error ocurred when attemp to make an http call.",
+          error: this.translate.instant("HTTP_PROCESS_ERROR"),
           internalError: error.message
         })
       });
